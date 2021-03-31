ee8f27f75999f91632feb61328e94e4083f13f61
DESKTOP-9VNHJEF\tietn <hungtn@amitech.vn>
2021-03-31 23:19:52 +0700

    add project

---
+++ src/main.js
@@ -0,0 +1,35 @@
+﻿import './main.scss';
+import m from "mithril"
+
+
+
+window.MediaManager = (function () {
+    
+    class MediaManager {
+        constructor(dataSetup) {
+            this.dataSetup = dataSetup;
+            this.events = {};
+            this.url = "";
+        }
+        on(type, callback) {
+            this.events[type] = this.events[type] || [];
+            this.events[type].push(callback);
+        }
+        open() {
+            m.render(root, <MyComponent/>)
+        }
+    }
+    return MediaManager;
+}());
+
+var root = document.body;
+
+function MyComponent() {
+    return {
+        view: () => (
+            <main>
+                <h1>Hello world</h1>
+            </main>
+        )
+    }
+}
---
+++ webpack.prod.js
@@ -0,0 +1,49 @@
+﻿const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    mode: 'production',
+    entry: {
+        filemanagerupload: ['./src/main.js']
+    },
+    output: {
+        filename:  '[name].min.js',
+        path: __dirname + '/dist'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /\/node_modules\//,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename:'[name].min.css',
+        }),
+    ]
+};
+
